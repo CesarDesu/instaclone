2d0b39ee54dce84f6c86c681f7af0d35e3d60913
Cesaru <cesaru.desu@gmail.com>
2021-01-03 19:50:14 -0500

    client initial

---
+++ client/src/config/apollo.js
@@ -0,0 +1,32 @@
+import {
+  ApolloClient,
+  createHttpLink,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
+import { setContext } from "apollo-link-context";
+import { getToken } from "../utils/token";
+
+const httpLik = createUploadLink({
+  uri: "http://localhost:4000/",
+});
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = getToken();
+
+//   return {
+//     headers: {
+//       ...headers,
+//       Authorization: token ? `Bearer ${token}` : "",
+//     },
+//   };
+// });
+
+const client = new ApolloClient({
+  connectToDevTools: true,
+  cache: new InMemoryCache(),
+  link: httpLik,
+});
+
+export default client;
---
+++ client/src/pages/Error404/Error404.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Error404.scss";
+
+export default function Error404() {
+  return (
+    <div>
+      <p>Error 404</p>
+    </div>
+  );
+}
