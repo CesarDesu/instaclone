ecbc808df734dcdc43e847112a75de32997aea4e
Cesaru <cesaru.desu@gmail.com>
2021-01-05 11:58:17 -0500

    deleted comment

--- client/src/config/apollo.js
+++ client/src/config/apollo.js
@@ -5,23 +5,10 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
-import { setContext } from "apollo-link-context";
-import { getToken } from "../utils/token";
 
 const httpLik = createUploadLink({
   uri: "http://localhost:4000/",
 });
-
-// const authLink = setContext((_, { headers }) => {
-//   const token = getToken();
-
-//   return {
-//     headers: {
-//       ...headers,
-//       Authorization: token ? `Bearer ${token}` : "",
-//     },
-//   };
-// });
 
 const client = new ApolloClient({
   connectToDevTools: true,
